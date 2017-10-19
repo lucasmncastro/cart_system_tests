037c8780a626eecb5fa279cfb4cfec64b43d8045
Lucas Castro <castro.lucas@gmail.com>
2017-10-19 10:38:51 -0300

    Adding index view with the products

--- spec/views/store/index.html.erb_spec.rb
+++ spec/views/store/index.html.erb_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "store/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "display all the products" do
+    products = [Product.create!(name: 'Book 1', price: 10), Product.create!(name: 'Book 2', price: 20)]
+    assign(:products, products)
+    render
+    expect(rendered).to include("Book 1") 
+    expect(rendered).to include("Book 2") 
+  end
 end
