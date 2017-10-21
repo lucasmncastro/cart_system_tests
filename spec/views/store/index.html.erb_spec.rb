ba4ee855a0cab070b9b7c5947ddfcab1968cdc44
Lucas Castro <castro.lucas@gmail.com>
2017-10-21 17:11:31 -0300

    Fixing typing

--- spec/views/store/index.html.erb_spec.rb
+++ spec/views/store/index.html.erb_spec.rb
@@ -47,7 +47,7 @@ RSpec.describe "store/index.html.erb", type: :view do
   end
 
   describe "when the user isn't logged in" do
-    it "display 'My cart' link" do
+    it "should display the login link" do
       products = [Product.create!(name: 'Book 1', price: 10), Product.create!(name: 'Book 2', price: 20)]
       assign(:products, products)
       assign(:current_user, nil)
